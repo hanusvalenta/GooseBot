7585a2c2607fe0589495edb5b2720c64f619bf0d
Hanus Valenta <hanus.valenta@gmail.com>
2023-04-10 01:12:06 +0200

    add random status

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { token } = require('./config.json');
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -10,8 +10,31 @@ const client = new Client({
   ],
 });
 
+let status = [
+  {
+    name: 'Online for some reason',
+    type: ActivityType.Streaming,
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  },
+  {
+    name: 'Online for some reason',
+    type: ActivityType.Watching,
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  },
+  {
+    name: 'Online for some reason',
+    type: ActivityType.Listening,
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  },
+];
+
 client.on('ready', (c) => {
   console.log(`${c.user.tag} is online.`);
+
+  setInterval(() => {
+    let random = Math.floor(Math.random() * status.length);
+    client.user.setActivity(status[random]);
+  }, 10000);
 });
 
 client.on('interactionCreate', (interaction) => {
