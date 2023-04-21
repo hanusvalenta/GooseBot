0711e6dc2c302387731282d2134d1fb9d9eeec1d
Hanus Valenta <hanus.valenta@gmail.com>
2023-04-21 23:11:46 +0200

    Y -> y

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let status = [
     url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
   },
   {
-    name: 'Your every move',
+    name: 'your every move',
     type: ActivityType.Watching,
   },
   {
